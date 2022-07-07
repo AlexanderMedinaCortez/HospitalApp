3995d5a7732cb916d748e95e8fc5207b89d3b2f2
Alexander Medina <80848709+AlexanderMedinaCortez@users.noreply.github.com>
2022-07-07 00:36:23 -0500

    implementacion de la vista de listado y busqueda de doctores

--- hospital-web/demo-hospital/src/main/java/com/example/controller/DoctorController.java
+++ hospital-web/demo-hospital/src/main/java/com/example/controller/DoctorController.java
@@ -2,11 +2,19 @@ package com.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.entities.Doctor;
 import com.example.service.CiudadService;
 import com.example.service.DoctorService;
+
+import antlr.collections.List;
 
 @Controller
 @RequestMapping("/doctors")
@@ -14,12 +22,48 @@ public class DoctorController {
 
 	@Autowired
 	private DoctorService doctorService;
-	
+
 	@Autowired
 	private CiudadService ciudadService;
+
+	@GetMapping("/new")
+	public String registerDoctor(Model model) {
+		model.addAttribute("doctor", new Doctor());
+		model.addAttribute("ciudades", ciudadService.listCity());
+		return "doctor/form";
+	}
 	
 	@GetMapping
-	public String listDoctors() {
+	public String listDoctors(Model model) {
+		model.addAttribute("doctor", new Doctor());
+		model.addAttribute("doctores", doctorService.listDoctor());
+		return "doctor/list";
+	}
+
+	@PostMapping("/register")
+	public String registerDoctor(@Validated Model model, @ModelAttribute Doctor doctor, BindingResult result) {
+		int rpta;
+		if (result.hasErrors()) {
+			model.addAttribute("ciudades", ciudadService.listCity());
+			return "doctor/form";
+		}
+
+		rpta=doctorService.RegisterDoctor(doctor);
+		
+		if(rpta>0) {
+			model.addAttribute("mensaje", "El numero de DNI ya existe");
+			model.addAttribute("ciudades", ciudadService.listCity());
+		} else {
+			model.addAttribute("mensaje", "El registro nuevo doctor");
+			model.addAttribute("doctor", new Doctor());
+			model.addAttribute("ciudades", ciudadService.listCity());
+		}
+		return "doctor/form";
+	}
+
+	@PostMapping("/find")
+	public String findDoctors(Model model, @ModelAttribute Doctor doctor) {
+		model.addAttribute("doctores", doctorService.findDoctorForDni(doctor.getDni()));
 		return "doctor/list";
 	}
 }
